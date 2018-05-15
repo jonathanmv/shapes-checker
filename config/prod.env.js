dfa88416364bd3aaef7c78d8f1d96126d5be998d
Jonathan Morales Vélez <jonathan@themidgame.com>
2018-05-15 13:35:27 +0100

    Added dev and prod bucket names

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,5 +3,5 @@ module.exports = {
   NODE_ENV: '"production"',
   AWS_USER_KEY: '"AKIAI5OUG6JIYX5ZYUJA"',
   AWS_USER_SECRET_KEY: '"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"',
-  S3_BUCKET_NAME: 'autenti.ca/trained-models/'
+  S3_BUCKET_NAME: '"autenti.ca/trained-models/"'
 }
